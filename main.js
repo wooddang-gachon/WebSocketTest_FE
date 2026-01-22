058a284f5146abe50348df61b6c142f922d6f734
wooddang-gachon <al457616@gachon.ac.kr>
2026-01-22 13:56:50 +0900

    서버 요청 및 반송값 확인 완료

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const checkLogin = document.getElementById("checkLogin");
 
 loginBtn.addEventListener("click", function () {
   checkLogin.innerText = "버튼이 눌렸고, 값이 변경되었습니다!"; // p 태그에 값 넣기
+  fetch("http://localhost:3000/contacts")
+    .then((response) => response.json()) // 1. 응답을 JSON으로 변환
+    .then((data) => {
+      // 2. 변환된 데이터를 화면에 뿌리기
+      console.log(data.date); // 브라우저 콘솔 확인용
+      document.getElementById("checkLogin").innerText = data.message;
+    })
+    .catch((error) => {
+      alert("에러가 났어요! 서버가 켜져 있나요?");
+    });
 });
 
 // function changeByJS() {
