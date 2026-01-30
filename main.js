0f66ec5d3b0e5e34d69b6a18b3b3d8b44f962195
wooddang-gachon <al457616@gachon.ac.kr>
2026-01-30 19:53:34 +0900

    sign in, sigh up 버튼 추가

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 function getIdPw() {
   let id = document.getElementById("ID").value;
   let pw = document.getElementById("PW").value;
-  return { id: id, pw: pw };
+  return { id, pw };
 }
 const serverUrl = "http://localhost:3000";
-const test = "/api/first-test/test";
+const test = "/api/auth/signIn";
 
 document
-  .getElementById("loginBtn")
+  .getElementById("logInBtn")
   .addEventListener("click", async function () {
+    console.log("data sent");
     const { id, pw } = getIdPw();
-
+    console.log(`id: ${id} pw: ${pw}`);
     const res = await fetch(serverUrl + test, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ id: id, pw: pw }),
+      body: JSON.stringify({ id, pw }),
+    });
+    const data = await res.json();
+    console.log(data);
+  });
+
+
+document
+  .getElementById("logUpBtn")
+  .addEventListener("click", async function () {
+    console.log("data sent");
+    const { id, pw } = getIdPw();
+    console.log(`id: ${id} pw: ${pw}`);
+    const res = await fetch(serverUrl + "/api/auth/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, pw }),
     });
     const data = await res.json();
     console.log(data);
